cf709553ea9a54dfb772b2d61704ad6df2c1e7d5
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-05-30 17:34:50 -0300

    Requisito 3 concluído

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -2,9 +2,9 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     displayName: DataTypes.STRING,
-    email: DataTypes.String,
-    password: DataTypes.String,
-    image: DataTypes.String,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
   },
   { timestamps: false });
 
--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
+const express = require('express');
 require('dotenv').config();
 const app = require('./api');
+const routes = require('./routes');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
+
+const apiRoutes = express.Router();
 
 // não remova esse endpoint
 app.get('/', (_request, response) => {
   response.send();
 });
+
+apiRoutes.post('/login', routes.login);
+
+app.use(apiRoutes);
 
 app.listen(port, () => console.log('ouvindo porta', port));
