7e932c20d16efa5d357f1873124a13313124eb41
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-06-06 15:49:47 -0300

    Requisito 10 completo - model blogPost e modificação do model user para adicionar o hasMany

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -8,5 +8,12 @@ module.exports = (sequelize, DataTypes) => {
   },
   { timestamps: false });
 
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId',
+      as: 'BlogPost'
+    })
+  }
+
   return User;
 };
